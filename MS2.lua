69b6958997df75771da864cef91e0283da0fbf1d
Nguyen04 <84752675+04Nguyen@users.noreply.github.com>
2022-06-29 08:25:41 +0700

    Create MS2.lua

---
+++ MS2.lua
@@ -0,0 +1,8 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+OrionLib:MakeNotification({
+    Name = "Game : Mining Simulator 2",
+    Content = "Loaded!",
+    Image = "rbxassetid://4483345998",
+    Time = 5
+})
