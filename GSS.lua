ae3f1434ef731a76e4ec2598fa5f4300b2f8826b
Nguyen04 <84752675+04Nguyen@users.noreply.github.com>
2022-06-29 08:26:24 +0700

    Create GSS.lua

---
+++ GSS.lua
@@ -0,0 +1,8 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+OrionLib:MakeNotification({
+    Name = "Game : Gas Station Simulator",
+    Content = "Loaded!",
+    Image = "rbxassetid://4483345998",
+    Time = 5
+})
